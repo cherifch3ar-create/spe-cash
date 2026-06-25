e06a03a4dce2d371e36d83126e1101b7ee779fc0
cherifch3ar-create <cherifch3ar@gmail.com>
2026-06-25 15:17:03 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'spe-cash-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).then((r) => {
+      const copy = r.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+      return r;
+    }).catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
