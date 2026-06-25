8205c85eaafd42b98d1eb98ff2a2fa1faf34c3e6
cherifch3ar-create <cherifch3ar@gmail.com>
2026-06-25 16:54:06 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-const CACHE = 'spe-cash-v1';
+const CACHE = 'spe-money-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
-  self.skipWaiting();
 });
-self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('message', (e) => { if (e.data === 'skipWaiting') self.skipWaiting(); });
 self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
   e.respondWith(
     fetch(e.request).then((r) => {
       const copy = r.clone();
