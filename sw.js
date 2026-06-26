aed90a61b08c714109831cc9f893186207408ba5
cherifch3ar-create <cherifch3ar@gmail.com>
2026-06-26 14:44:00 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spe-money-v6';
+const CACHE = 'spe-money-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', (e) => {
   self.skipWaiting();
